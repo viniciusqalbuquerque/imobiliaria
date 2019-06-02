f128894a0a3521d25d1565d5c4d8babe0eba6e2e
Your Name <you@example.com>
2019-06-02 09:46:52 -0300

    choices para uf e status emovel

--- src/Forms/EnderecoType.php
+++ src/Forms/EnderecoType.php
@@ -7,8 +7,10 @@ use App\Entity\Endereco;
 use App\Entity\Usuario;
 use function PHPSTORM_META\type;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -41,8 +43,37 @@ class EnderecoType extends AbstractType
             ->add('complemento', TextType::class, [
                 'label' => 'Complemento',
             ])
-            ->add('uf', TextType::class, [
+            ->add('uf', ChoiceType::class, [
                 'label' => 'UF:',
+                'choices' => [
+                    'Acre' => 'AC',
+                    'Alagoas' => 'AL',
+                    'Amazonas' => 'AM',
+                    'Amapá' => 'AP',
+                    'Bahia' => 'BA',
+                    'Ceará' => 'CE',
+                    'Distrito Federal' => 'DF',
+                    'Espírito Santo' => 'ES',
+                    'Goiás' => 'GO',
+                    'Maranhão' => 'MA',
+                    'Mato Grosso' => 'MT',
+                    'Mato Grosso do Sul' => 'MS',
+                    'Minas Gerais' => 'MG',
+                    'Pará' => 'PA',
+                    'Paraíba' => 'PB',
+                    'Paraná' => 'PR',
+                    'Pernambuco' => 'PE',
+                    'Piauí' => 'PI',
+                    'Rio de Janeiro' => 'RJ',
+                    'Rio Grande do Norte' => 'RN',
+                    'Rio Grande do Sul' => 'RS',
+                    'Rondônia' => 'RO',
+                    'Roraima' => 'RR',
+                    'Santa Catarina' => 'SC',
+                    'São Paulo' => 'SP',
+                    'Sergipe' => 'SE',
+                    'Tocantins' => 'TO'
+                ],
             ])
             ->add('cidade', TextType::class, [
                 'label' => 'Cidade:',
@@ -50,7 +81,7 @@ class EnderecoType extends AbstractType
             ->add('dddTelefone', TextType::class, [
                 'label' => 'DDD:',
             ])
-            ->add('telefone', TextType::class, [
+            ->add('telefone', TelType::class, [
                 'label' => 'Telefone:',
             ])
             ->add('dddCelular', TextType::class, [
--- src/Forms/ImovelType.php
+++ src/Forms/ImovelType.php
@@ -6,6 +6,7 @@ namespace App\Forms;
 use App\Entity\Imovel;
 use App\Entity\Usuario;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -27,8 +28,17 @@ class ImovelType extends AbstractType
         $builder
 //            ->setAction('../usuario')
 //            ->setMethod('POST')
-            ->add('status', TextType::class, [
-                'label' => 'Status'
+            ->add('status', ChoiceType::class, [
+                'label' => 'Status',
+                'choices' => [
+                    'Disponivel' => 'disponivel',
+                    'Alugado' => 'alugado',
+                    'Em Manutenção' => 'Em Manutenção',
+                    'Vistoria' => 'Vistoria',
+                    'Sem Cotrato Adm' => 'Sem Cotrato Adm',
+                    'Sem Contrato de Locação' => 'Sem Contrato de Locação'
+                ]
+
             ])
             ->add('caracteristicas', TextType::class, [
                 'label' => 'Caracterisiticas do Imovel',
@@ -36,8 +46,16 @@ class ImovelType extends AbstractType
             ->add('observacao', TextType::class, [
                 'label' => 'Observações Geral',
             ])
-            ->add('tipoImovel', TextType::class, [
-                'label' => 'Tipo do Imovel',
+            ->add('tipoImovel', ChoiceType::class, [
+                'empty_data' => 'Casa',
+                'choices' => [
+                    'Casa' => 'Casa',
+                    'Apartamento' => 'Apartamento',
+                    'Galpão' => 'Calpão',
+                    'Terreno' => 'Terreno',
+                    'Prédio Comercial' => 'Prédio Comercial',
+                    'Loja em Shopping' => 'Loja em Shopping'
+                ]
             ])
             ->add('dtCadastro', DateType::class, [
                 'label' => 'Data de Cadastro',
